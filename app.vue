26ef73a0355b7123df64a1f4adc06b79194f3a5c
Hans <classsickpeople@gmail.com>
2023-03-01 16:33:43 +0800

    Create fixed header

--- app.vue
+++ app.vue
@@ -1,10 +1,42 @@
 <template>
-    <NuxtLayout>
-        <main>
-            <NuxtPage />
-        </main>
-    </NuxtLayout>
+    <div :class="{'scrolled' : scrollY > 0}">
+        <NuxtLayout>
+            <main>
+                <NuxtPage />
+            </main>
+        </NuxtLayout>
+    </div>
 </template>
+
+<script lang="ts">
+export default {
+    data () {
+        return {
+            scrollY: 0
+        }
+    },
+    created () {
+        // eslint-disable-next-line no-undef
+        if (process.client) { 
+            window.addEventListener('scroll', this.handleScroll)
+        }
+    },
+    unmounted () {
+        // eslint-disable-next-line no-undef
+        if (process.client) { 
+            window.removeEventListener('scroll', this.handleScroll)
+        }
+    },
+    methods: {
+        handleScroll () {
+            // eslint-disable-next-line no-undef
+            if (process.client) {
+                this.scrollY = window.scrollY
+            }
+        }
+    }
+}
+</script>
 
 <style lang="scss">
     * {
@@ -21,5 +53,20 @@
     .page-enter-from,
     .page-leave-to {
         opacity: 0;
+    }
+
+    .header-border {
+        font-size: 32px;
+        line-height: 44px;
+        font-weight: 300;
+        
+        &:before {
+            content: "";
+            display: block;
+            width: 48px;
+            height: 3px;
+            margin-bottom: 24px;
+            background: $primary-color;
+        }
     }
 </style>
