8ef0a83a55d042469cb7a58cb93d8a013a612605
Hans <classsickpeople@gmail.com>
2023-03-05 20:30:32 +0800

    Update style file extension

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
             }
         }
     },
-    css: ['~/assets/styles/main.css'],
+    css: ['~/assets/styles/main.scss'],
     postcss: {
         plugins: {
             tailwindcss: {},
