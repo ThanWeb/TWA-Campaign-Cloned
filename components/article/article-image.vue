98b601c5d5c4fcdde8086b82cc2b79fd36285ab5
Hans <classsickpeople@gmail.com>
2023-03-14 00:08:11 +0800

    Create article header and article image component

---
+++ components/article/article-image.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="my-10 px-4">
+        <img 
+            :src="`/images/articles/${image}`" 
+            :alt="imageDescription"
+            class="mb-2"
+        >
+        <p class="text-xs font-normal text-gray-darkest">
+            {{ imageDescription }}
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        image: {
+            type: String,
+            default: 'article-01.png'
+        },
+        imageDescription: {
+            type: String,
+            default: 'Lorem ipsum image description'
+        }
+    }
+}
+</script>
