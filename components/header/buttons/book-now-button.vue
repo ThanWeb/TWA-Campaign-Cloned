9c3b0ba4022cdd9bbedf1c96f581f5dfc5547c66
Hans <classsickpeople@gmail.com>
2023-03-15 12:29:56 +0800

    Create new component in header

---
+++ components/header/buttons/book-now-button.vue
@@ -0,0 +1,8 @@
+<template>
+    <a
+        href="#book-now-section"
+        class="book-now-header hidden lg:block lg:ml-auto lg:w-24 rounded-3xl book-now-button mx-3 uppercase py-3 font-semibold tracking-widest text-center"
+    >
+        Book Now
+    </a>
+</template>
---
+++ components/header/buttons/open-close-nav.vue
@@ -0,0 +1,20 @@
+<template>
+    <button type="button">
+        <img 
+            :src="isMainNavShowed ? '/images/icons/close.svg' : '/images/icons/hamburger.svg'"
+            alt="Main Menu"
+            :class="isMainNavShowed ? 'close-nav' : 'open-nav'"
+        >
+    </button>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        isMainNavShowed: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
